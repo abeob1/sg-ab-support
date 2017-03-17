eebead9df4f039ceedd92319680fddf9d5cf4e48
abeob1 <sapb1.abeoelectra@gmail.com>
2017-03-17 08:26:34 +0530

    Ticket Source Code

---
+++ Ticket Source Code/IMAP/IMAP/EmailReader.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using D.Net.EmailInterfaces;
+using D.Net.EmailClient;
+using System.Configuration;
+using System.Data;
+
+namespace IMAP
+{
+    public class EmailReader
+    {
+        static void Main(string[] args)
+        {
+            string sFuncName = string.Empty;
+            clsLog oLog = new clsLog();
+            Logic oLogic = new Logic();
+            string sErrDesc = string.Empty;
+
+            try
+            {
+                sFuncName = "Main Program";
+                oLog.WriteToDebugLogFile("Starting Program", sFuncName);
+                string sTicketNumber = string.Empty;
+                string sBodyContent = string.Empty;
+                //SupportPortalService.SupportSoapClient oSupportPortal = new SupportPortalService.SupportSoapClient();
+
+                string sServer = ConfigurationManager.AppSettings["Server"];
+                int iPort = Convert.ToInt32(ConfigurationManager.AppSettings["Port"]);
+                string sFromEmail = ConfigurationManager.AppSettings["FromEmail"];
+                string sFromPassword = ConfigurationManager.AppSettings["FromPassword"];
+
+                IEmailClient ImapClient = EmailClientFactory.GetClient(EmailClientEnum.IMAP);
+                ImapClient.Connect(sServer, sFromEmail, sFromPassword, iPort, true);
+                ImapClient.SetCurrentFolder("INBOX");
+                ImapClient.LoadMessages();
+                string sLastEmailId = oLogic.GetMailNumber();
+                if (sLastEmailId != "NO RECORDS")
+                {
+                    int iMailId = Convert.ToInt32(sLastEmailId);
+                    for (int i = 440; i <= ImapClient.Messages.Count - 1; i++)
+                    {
+                        IEmail msm = (IEmail)ImapClient.Messages[i];
+                        if (msm.SequenceNumber > iMailId)
+                        {
+                            msm.LoadInfos();
+                            // Check the subject contains ticket ID or nor
+                            if (msm.Subject.Contains("[##"))
+                            {
+                                sTicketNumber = oLogic.Between(msm.Subject, "[##", "##]");
+                                sBodyContent = msm.TextBody.ToString().Replace("\r\n", "<br/>");
+                                //call create thread
+                                string sResult = oLogic.InsertTicketThread(sTicketNumber, sBodyContent);
+                            }
+                            else
+                            {
+                                string sEmail = msm.From[0];
+                                string sUserName = oLogic.Before(sEmail, "@");
+                                string sResult = oLogic.InsertTicketandUser(sUserName, sEmail);
+                            }
+                            Console.WriteLine(msm.Date.ToString() + " - " + msm.From[0] + " - " +
+                                              msm.Subject + " - " + msm.TextBody + " - " + msm.Attachments.Count);
+                        }
+                    }
+                    oLog.WriteToDebugLogFile("Ending Program", sFuncName);
+                }
+                else
+                {
+                    oLog.WriteToDebugLogFile(sLastEmailId, sFuncName);
+                }
+
+            }
+            catch (Exception Ex)
+            {
+                sErrDesc = Ex.Message.ToString();
+                oLog.WriteToErrorLogFile(sErrDesc, sFuncName);
+                oLog.WriteToDebugLogFile("Completed With ERROR  ", sFuncName);
+            }
+
+        }
+    }
+}
---
+++ Ticket Source Code/IMAP/IMAP/Logic.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Configuration;
+
+namespace IMAP
+{
+    public class Logic
+    {
+        public string GetMailNumber()
+        {
+            clsLog oLog = new clsLog();
+            string sErrDesc = string.Empty;
+
+            string sFuncName = "GetMailNumber";
+            string sSQL = string.Empty;
+            DataTable oRetDt = new DataTable();
+            string sResult = string.Empty;
+            string ConnectionString = ConfigurationManager.ConnectionStrings["dbconnection"].ConnectionString;
+
+            try
+            {
+                oLog.WriteToDebugLogFile("Starting Function", sFuncName);
+
+                sSQL = "select IsNull(last_mail_B1,'0') MailNumber from mailnumber";
+                oRetDt = Functions.ExecuteDatatable(ConnectionString, CommandType.Text, sSQL);
+                if (oRetDt.Rows.Count > 0)
+                {
+                    sResult = oRetDt.Rows[0]["MailNumber"].ToString();
+                    oLog.WriteToDebugLogFile("Completed with SUCCESS", sFuncName);
+                }
+                else
+                {
+                    sResult = "NO RECORDS";
+                }
+            }
+            catch (Exception ex)
+            {
+                sErrDesc = ex.Message.ToString();
+                sResult = ex.Message.ToString();
+                oLog.WriteToErrorLogFile(sErrDesc, sFuncName);
+                oLog.WriteToDebugLogFile("Completed With ERROR", sFuncName);
+            }
+            return sResult;
+        }
+
+        public string InsertTicketThread(string sTicketNum, string sBodyContent)
+        {
+            clsLog oLog = new clsLog();
+            string sErrDesc = string.Empty;
+
+            string sFuncName = "InsertTicketThread";
+            string sProcName = string.Empty;
+            DataTable oRetDt = new DataTable();
+            string sResult = string.Empty;
+            string ConnectionString = ConfigurationManager.ConnectionStrings["dbconnection"].ConnectionString;
+
+            try
+            {
+                oLog.WriteToDebugLogFile("Starting Function", sFuncName);
+
+                sProcName = "AE_SP001_SupportPortal_InsertTicketThread";
+                oRetDt = Functions.ExecuteDataSet(ConnectionString, CommandType.StoredProcedure, sProcName, Data.CreateParameter("@TicketNum", sTicketNum),
+                     Data.CreateParameter("@BodyContent", sBodyContent)).Tables[0];
+                if (oRetDt.Rows.Count > 0)
+                {
+                    sResult = oRetDt.Rows[0]["Message"].ToString();
+                    oLog.WriteToDebugLogFile("Completed with SUCCESS", sFuncName);
+                }
+                else
+                {
+                    sResult = "NO RECORDS";
+                }
+            }
+            catch (Exception ex)
+            {
+                sErrDesc = ex.Message.ToString();
+                sResult = ex.Message.ToString();
+                oLog.WriteToErrorLogFile(sErrDesc, sFuncName);
+                oLog.WriteToDebugLogFile("Completed With ERROR", sFuncName);
+            }
+            return sResult;
+        }
+
+        public string InsertTicketandUser(string sUserName, string sEmail)
+        {
+            clsLog oLog = new clsLog();
+            string sErrDesc = string.Empty;
+
+            string sFuncName = "InsertTicketandUser";
+            string sProcName = string.Empty;
+            DataTable oRetDt = new DataTable();
+            string sResult = string.Empty;
+            string ConnectionString = ConfigurationManager.ConnectionStrings["dbconnection"].ConnectionString;
+
+            try
+            {
+                oLog.WriteToDebugLogFile("Starting Function", sFuncName);
+
+                sProcName = "AE_SP002_SupportPortal_InsertTicketandUser";
+                oRetDt = Functions.ExecuteDataSet(ConnectionString, CommandType.StoredProcedure, sProcName, Data.CreateParameter("@UserName", sUserName),
+                     Data.CreateParameter("@Email", sEmail)).Tables[0];
+                if (oRetDt.Rows.Count > 0)
+                {
+                    sResult = oRetDt.Rows[0]["Message"].ToString();
+                    oLog.WriteToDebugLogFile("Completed with SUCCESS", sFuncName);
+                }
+                else
+                {
+                    sResult = "NO RECORDS";
+                }
+            }
+            catch (Exception ex)
+            {
+                sErrDesc = ex.Message.ToString();
+                sResult = ex.Message.ToString();
+                oLog.WriteToErrorLogFile(sErrDesc, sFuncName);
+                oLog.WriteToDebugLogFile("Completed With ERROR", sFuncName);
+            }
+            return sResult;
+        }
+
+        public string Between(string value, string a, string b)
+        {
+            int posA = value.IndexOf(a);
+            int posB = value.LastIndexOf(b);
+            if (posA == -1)
+            {
+                return "";
+            }
+            if (posB == -1)
+            {
+                return "";
+            }
+            int adjustedPosA = posA + a.Length;
+            if (adjustedPosA >= posB)
+            {
+                return "";
+            }
+            return value.Substring(adjustedPosA, posB - adjustedPosA);
+        }
+
+        public string Before(string value, string a)
+        {
+            int posA = value.IndexOf(a);
+            if (posA == -1)
+            {
+                return "";
+            }
+            return value.Substring(0, posA);
+        }
+
+        public string After(string value, string a)
+        {
+            int posA = value.LastIndexOf(a);
+            if (posA == -1)
+            {
+                return "";
+            }
+            int adjustedPosA = posA + a.Length;
+            if (adjustedPosA >= value.Length)
+            {
+                return "";
+            }
+            return value.Substring(adjustedPosA);
+        }
+    }
+}
